2ed9f6e1b4729b3552c39ac1c452ec9eab43436e
paramuececse <101621641+paramuececse@users.noreply.github.com>
2023-08-28 20:02:14 +0530

    Update SpicetJetListeners.java

--- src/main/java/SpicetJetListeners/SpicetJetListeners.java
+++ src/main/java/SpicetJetListeners/SpicetJetListeners.java
@@ -13,31 +13,27 @@ import Com.SpiceJet.DriverClass.DriverClassSpiceJet;
 public class SpicetJetListeners extends DriverClassSpiceJet implements ITestListener {
 WebDriver driver;
 ExtentReports extentReport=ExtentReport.getExtentReport();
-ExtentTest extentTest;
+public static ExtentTest extentTest;
 @Override
 public void onTestStart(ITestResult result) {
 	extentTest=extentReport.createTest(result.getName());
 }
 @Override
 public void onTestSuccess(ITestResult result) {
-	//log the status 
+	
 	extentTest.log(Status.PASS, "Test Passed");
-	String testName=result.getName();
-try {
-		
-		String screenshotpath= takeScreenshot(testName, driver);
-		extentTest.addScreenCaptureFromPath(screenshotpath);
-			} catch (IOException e) {
-		e.printStackTrace();
-	}
+
+	//String testName=result.getName();
+
+
 }
-//Invoked each time a test fails.
+
 @Override
 public void onTestFailure(ITestResult result) {
 	extentTest.fail(result.getThrowable());
-	//get the name of failed test case
+	
 	String testName=result.getName();
-	//Logic for screenshot capture
+	
 	try {
 		driver=(WebDriver) result.getTestClass().getRealClass().getField("driver").get(result.getInstance());
 	} 
